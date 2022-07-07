5b5b460c3cd49ee5d33c21ba362dec56c335c304
AKHTAR ALI <56265099+AKHTARALI879@users.noreply.github.com>
2022-07-07 05:44:16 +0300

    Add files via upload

---
+++ print_colored_text.py
@@ -0,0 +1,8 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.YELLOW + Back.BLUE + "THIS IS AKHTAR ALI")
+print(Fore.CYAN + Back.YELLOW + "Do not forget to like")
+print(Fore.GREEN + Back.BLUE + "& subscribe CSA")
+# let's run our code
